92de2e991bcb6c9a50303d5127e4a000d1ea8e92
1h3a3x7 <44430306+1h3a3x7@users.noreply.github.com>
2024-10-15 19:44:47 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+	site: 'https://sdmodding.github.io/',
+	base: 'TheoryEngine.Docs',
 	integrations: [
 		starlight({
 			favicon: '/favicon.png',

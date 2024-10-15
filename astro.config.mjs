381c3db312a7c6365eadf4d9c397e257f40d8c97
sneakyevil <sneakyevildogs@gmail.com>
2024-10-15 18:56:49 +0200

    Setup

---
+++ astro.config.mjs
@@ -0,0 +1,40 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+export default defineConfig({
+	integrations: [
+		starlight({
+			favicon: '/favicon.png',
+			title: 'Theory Engine Docs',
+			lastUpdated: true,
+			pagination: true,
+			logo: {
+				src: './src/assets/logo.png',
+				replacesTitle: true,
+			},
+			customCss: [
+			  './src/styles/custom.css',
+			],
+			social: {
+				github: 'https://github.com/SDmodding/TheoryEngine.Docs',
+			},
+			editLink: {
+				baseUrl: 'https://github.com/SDmodding/TheoryEngine.Docs/edit/main/',
+			},
+			components: {
+				ThemeProvider: './src/components/ThemeProvider.astro',
+				ThemeSelect: './src/components/ThemeSelect.astro',
+			},
+			sidebar: [
+				{
+					label: 'Illusion',
+					autogenerate: { directory: 'illusion' },
+				},
+				{
+					label: 'Quark',
+					autogenerate: { directory: 'quark' },
+				},
+			],
+		}),
+	],
+});
---
+++ src/content/docs/index.md
@@ -0,0 +1,8 @@
+---
+title: Welcome to Theory Engine docs
+template: doc
+tableOfContents: false
+editUrl: false
+---
+
+![Alt text](../../assets/wip.png)

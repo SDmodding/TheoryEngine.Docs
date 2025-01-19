6ba303f5b45a63ed685849615ca3ed83d2ab385f
sneakyevil <sneakyevildogs@gmail.com>
2025-01-20 00:07:14 +0100

    New theme & update to new packages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,39 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import ThemeRapide from './plugins/theme-rapide'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import icon from 'astro-icon';
+import ThemeObsidian from "./plugins/theme-obsidian"
 
 export default defineConfig({
-	site: 'https://sdmodding.github.io/',
-	base: 'TheoryEngine.Docs',
+	site: "https://sdmodding.github.io/",
+	base: "TheoryEngine.Docs",
 	integrations: [
+		icon(),
 		starlight({
-			plugins: [ThemeRapide()],
-			favicon: '/favicon.png',
-			title: 'Theory Engine Docs',
+			plugins: [ThemeObsidian()],
+			favicon: "/favicon.png",
+			title: "Theory Engine Docs",
 			lastUpdated: true,
 			pagination: true,
+            customCss: [
+				"@fontsource/inter/400.css",
+				"@fontsource/inter/600.css",
+                "./src/styles/global.css"
+            ],
 			logo: {
-				src: './src/assets/logo.png',
+				src: "./src/assets/logo.png",
 				replacesTitle: true,
 			},
 			social: {
-				github: 'https://github.com/SDmodding/TheoryEngine.Docs',
+				github: "https://github.com/SDmodding/TheoryEngine.Docs",
 			},
 			editLink: {
-				baseUrl: 'https://github.com/SDmodding/TheoryEngine.Docs/edit/main/',
+				baseUrl: "https://github.com/SDmodding/TheoryEngine.Docs/edit/main/",
 			},
 			components: {
 			},
 			sidebar: [
-				{
-					label: 'Illusion',
-					autogenerate: { directory: 'illusion' },
-				},
-				{
-					label: 'Quark',
-					autogenerate: { directory: 'quark' },
-				},
+				{ label: "[book] Quark", autogenerate: { directory: "quark" } },
+				{ label: "[cube] Illusion", autogenerate: { directory: "illusion" } },
 			]
 		}),
 	],

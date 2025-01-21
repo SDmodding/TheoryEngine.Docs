ef39da1c12eb84602a10c44e7786593a55c9618c
sneakyevil <sneakyevildogs@gmail.com>
2025-01-22 00:11:36 +0100

    Add image zoom, remove vue, fix margin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightImageZoom from 'starlight-image-zoom'
 import icon from 'astro-icon';
 import ThemeObsidian from "./plugins/theme-obsidian"
 
@@ -9,7 +10,7 @@ export default defineConfig({
 	integrations: [
 		icon(),
 		starlight({
-			plugins: [ThemeObsidian()],
+			plugins: [ starlightImageZoom(), ThemeObsidian() ],
 			favicon: "/favicon.png",
 			title: "Theory Engine Docs",
 			lastUpdated: true,
@@ -32,9 +33,9 @@ export default defineConfig({
 			components: {
 			},
 			sidebar: [
-				{ label: "[book] Quark", autogenerate: { directory: "quark" } },
-				{ label: "[cube] Illusion", autogenerate: { directory: "illusion" } },
-				{ label: "[gamepad] Sleeping Dogs", autogenerate: { directory: "sd" } },
+				{ label: "[book] Quark", collapsed: true, autogenerate: { directory: "quark" } },
+				{ label: "[cube] Illusion", collapsed: true, autogenerate: { directory: "illusion" } },
+				{ label: "[gamepad] Sleeping Dogs", collapsed: true, autogenerate: { directory: "sd" } },
 			]
 		}),
 	],
